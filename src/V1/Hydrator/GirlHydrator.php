521f491be84e961969247d14ba718d85411b8b50
visa4 <antonino.visalli@gmail.com>
2016-05-23 14:46:41 +0200

    Fix config

--- src/V1/Hydrator/GirlHydrator.php
+++ src/V1/Hydrator/GirlHydrator.php
@@ -4,7 +4,7 @@ namespace Strapieno\NightClubGirl\Api\V1\Hydrator;
 use Matryoshka\Model\Hydrator\Strategy\HasOneStrategy;
 use Matryoshka\Model\Hydrator\Strategy\SetTypeStrategy;
 use Strapieno\NightClub\Model\Entity\Object\AggregateRatingObject;
-use Strapieno\NightClubReview\Model\Entity\Object\NightClubReferenceObject;
+use Strapieno\NightClub\Model\Entity\Reference\NightClubReference;
 use Strapieno\Utils\Hydrator\DateHystoryHydrator;
 use Strapieno\Utils\Hydrator\Strategy\NamingStrategy\MapUnderscoreNamingStrategy;
 use Strapieno\Utils\Hydrator\Strategy\ReferenceEntityCompressStrategy;
@@ -29,7 +29,7 @@ class GirlHydrator extends DateHystoryHydrator
 
         $this->addStrategy(
             'nightclub_id',
-            new ReferenceEntityCompressStrategy(new NightClubReferenceObject(), false)
+            new ReferenceEntityCompressStrategy(new NightClubReference(), false)
         );
     }
 }
