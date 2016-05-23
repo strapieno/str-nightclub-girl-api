2666283d27e6f510a40bb0d6958141342fc6f877
visa4 <antonino.visalli@gmail.com>
2016-05-23 13:00:59 +0200

    Fix night club reference name strategy

--- src/V1/Hydrator/GirlHydrator.php
+++ src/V1/Hydrator/GirlHydrator.php
@@ -1,12 +1,35 @@
 <?php
 namespace Strapieno\NightClubGirl\Api\V1\Hydrator;
 
+use Matryoshka\Model\Hydrator\Strategy\HasOneStrategy;
+use Matryoshka\Model\Hydrator\Strategy\SetTypeStrategy;
+use Strapieno\NightClub\Model\Entity\Object\AggregateRatingObject;
+use Strapieno\NightClubReview\Model\Entity\Object\NightClubReferenceObject;
 use Strapieno\Utils\Hydrator\DateHystoryHydrator;
+use Strapieno\Utils\Hydrator\Strategy\NamingStrategy\MapUnderscoreNamingStrategy;
+use Strapieno\Utils\Hydrator\Strategy\ReferenceEntityCompressStrategy;
 
 /**
  * Class GirlHydrator
  */
 class GirlHydrator extends DateHystoryHydrator
 {
+    public function __construct($underscoreSeparatedKeys = true)
+    {
+        parent::__construct($underscoreSeparatedKeys);
+        // Naming strategy
+        $this->setNamingStrategy(new MapUnderscoreNamingStrategy(['nightclub_id' => 'nightClubReference']));
+        // Property strategy
+        $aggregateRating = new AggregateRatingObject();
+        $aggregateRating->getHydrator()->addStrategy('partial', new SetTypeStrategy('array', 'array'));
+        $this->addStrategy(
+            'aggregate_rating',
+            new HasOneStrategy($aggregateRating, false)
+        );
 
+        $this->addStrategy(
+            'nightclub_id',
+            new ReferenceEntityCompressStrategy(new NightClubReferenceObject(), false)
+        );
+    }
 }
